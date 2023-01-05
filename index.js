48f8a385f6597139312c3872cc13255ee12cea6c
byykkonen <byykkonen.av@gmail.com>
2023-01-04 19:17:02 -0600

    Node Intro

---
+++ index.js
@@ -0,0 +1,9 @@
+let productName = "Mentos";
+
+let piecesofGum = "50";
+
+console.log(piecesofGum);
+
+console.log(productName + " Jon has " + piecesofGum);
+
+console.log(`${productName} please jon ${piecesofGum}`);
